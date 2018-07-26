43795d8109696c2baf5916c92d0393fd16c7217b
GBTC440008UR <GBTC440008UR@MC.MONTGOMERYCOLLEGE.EDU>
2018-07-26 13:20:31 -0400

    Initial Commit

---
+++ src/com/company/GetIntegersSolution.java
@@ -0,0 +1,19 @@
+package com.company;
+
+import java.util.Scanner;
+
+public class GetIntegersSolution {
+    public void main (String[] args)
+    {
+        Scanner keyboard = new Scanner(System.in);
+
+        int num, total = 0;
+
+        for (int i = 1; i <= 10; i++)
+        {
+            num = keyboard.nextInt();
+            total += num;
+        }
+        System.out.println("The total of all 10 numbers is " + total);
+    }
+}
